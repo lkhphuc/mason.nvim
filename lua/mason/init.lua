35191f1e916f1396cb69ab16fcbcbb96634fed44
William Boman <william@redwill.se>
2022-07-24 15:11:45 +0200

    ui: show alpha branch message

--- lua/mason/init.lua
+++ lua/mason/init.lua
@@ -1,11 +1,13 @@
 local settings = require "mason.settings"
 local path = require "mason-core.path"
 local platform = require "mason-core.platform"
+local notify = require "mason-core.notify"
 
 local M = {}
 
 ---@param config MasonSettings | nil
 function M.setup(config)
+    notify("alpha branch is no longer active, switch to default branch (main).", vim.log.levels.WARN)
     if config then
         settings.set(config)
     end
--- lua/mason/ui/components/header.lua
+++ lua/mason/ui/components/header.lua
@@ -6,24 +6,18 @@ return function(state)
     return Ui.CascadingStyleNode({ "CENTERED" }, {
         Ui.HlTextNode {
             Ui.When(state.view.is_showing_help, {
-                p.none "             ",
                 p.header_secondary(" " .. state.header.title_prefix .. " mason.nvim "),
-                p.Comment " alpha branch",
                 p.none((" "):rep(#state.header.title_prefix + 1)),
             }, {
-                p.none "             ",
                 p.header " mason.nvim ",
-                p.Comment " alpha branch",
             }),
             Ui.When(
                 state.view.is_showing_help,
                 { p.none "        press ", p.highlight_secondary "?", p.none " for package list" },
                 { p.none "press ", p.highlight "?", p.none " for help" }
             ),
+            { p.WarningMsg " alpha branch is no longer active, switch to default branch (main)", },
             { p.Comment "https://github.com/williamboman/mason.nvim" },
-            {
-                p.Comment "Give usage feedback: https://github.com/williamboman/mason.nvim/discussions/new?category=ideas",
-            },
         },
     })
 end
